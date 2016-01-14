940b11e75d33b899fe949d398c49263d42ad7bec
Sam Peacey <sam.peacey@gmail.com>
2016-01-14 16:34:49 +0900

    made the logged in account and log out links a dropdown

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,15 @@ class ApplicationController < ActionController::Base
 
   helper_method :resource_name, :resource, :devise_mapping
 
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) << :name
+    devise_parameter_sanitizer.for(:account_update) << :name
+  end
+
   def resource_name
     :user
   end
