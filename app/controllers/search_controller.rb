2c5c0a4c266b81aed5101572db1301e4f1d90555
Sam Peacey <sam.peacey@gmail.com>
2016-01-17 21:30:50 +0900

    bit of a tidy up and cached categories

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,6 +2,7 @@ class SearchController < ApplicationController
 
   def index
     @filters = SearchFilter.build params
+    @categories = Category.includes(:sub_categories).all
     @category = Category.find_by_id(params[:category_id]) if params[:category_id]
     @products = Product.search(params[:search], params[:category_id], params[:sub_category_id], params[:company_id]) 
     current_user.remove_search( params[:delete_search] ) if params[:delete_search]
