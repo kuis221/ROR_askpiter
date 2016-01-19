81be844fbee5feeca97fd379ce466db8f21ebd70
Antoine Berton <antoine@angeltech.io>
2016-01-19 15:07:40 +0000

    added geocoder gem

--- Gemfile
+++ Gemfile
@@ -14,6 +14,7 @@ gem 'bootstrap-sass'
 gem 'devise'
 gem 'high_voltage'
 gem 'activeadmin', github: 'activeadmin/activeadmin'
+gem 'geocoder'
 
 group :development, :test do
   gem 'byebug'
--- app/models/address.rb
+++ app/models/address.rb
@@ -1,3 +1,8 @@
 class Address < ActiveRecord::Base
   belongs_to :addressable, polymorphic: true
+  def full_address
+    "#{street}, #{city}"
+  end
+  geocoded_by :full_address
+  after_validation :geocode
 end
