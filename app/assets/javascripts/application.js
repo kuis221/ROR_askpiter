25e74022346e6bf283d86b896e620eef21762d84
Alexander Panasyuk <panasmeister@gmail.com>
2016-01-19 18:33:26 +0700

    ActiveAdmin Assets fix

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,4 +15,4 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require bootstrap-sprockets
-//= require_tree .
+//= require_directory .
--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -185,13 +185,15 @@ ActiveAdmin.setup do |config|
   # and feel.
   #
   # To load a stylesheet:
-  #   config.register_stylesheet 'my_stylesheet.css'
+  config.clear_stylesheets!
+  config.register_stylesheet 'active_admin/active_admin.css'
   #
   # You can provide an options hash for more control, which is passed along to stylesheet_link_tag():
   #   config.register_stylesheet 'my_print_stylesheet.css', media: :print
   #
   # To load a javascript file:
-  #   config.register_javascript 'my_javascript.js'
+  config.clear_javascripts!
+  config.register_javascript 'active_admin/active_admin.js'
 
   # == CSV options
   #
