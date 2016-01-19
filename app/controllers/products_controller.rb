7b0d73c4fa6f77dd39982f70810e5bd90488ebf5
Alexander Zagaynov <zalex80@gmail.com>
2016-01-20 04:19:11 +0700

    similar products section added

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,14 +1,14 @@
 class ProductsController < ApplicationController
   include HelperAttr
-  helper_attr :product, :pictures, :picture, :reviews, :photos, :videos
+  helper_attr :product, :pictures, :reviews, :photos, :videos, :similars
 
   def show
     set_product
     self.pictures = product.pictures.recent.first(6)
-    self.picture = pictures.first || product.default_picture
     self.reviews = product.reviews.recent.first(3)
     self.photos = product.photos.recent.first(5)
     self.videos = product.videos.recent.first(5)
+    self.similars = product.similars.recent.first(10)
   end
 
   private
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,9 @@
 class Product < ActiveRecord::Base
+  include Recentable
+
   belongs_to :company
-  belongs_to :sub_category
+  belongs_to :sub_category, inverse_of: :products
+
   has_many :videos
   has_many :photos
   has_many :prices
@@ -11,6 +14,7 @@ class Product < ActiveRecord::Base
   has_many :user_favourites, as: :favouriteable
   has_many :users, through: :user_favourites
   has_many :pictures, as: :imageable
+  has_many :similars, -> (p) { where.not(id: p) }, through: :sub_category, source: :products
 
   alias_attribute :category, :sub_category # .category method
   alias_attribute :characteristics, :attrs # .characteristics method
@@ -19,7 +23,9 @@ class Product < ActiveRecord::Base
   delegate :name, to: :category, prefix: true # .category_name method
   delegate :count, to: :photos, prefix: true # .photos_count method
   delegate :count, to: :videos, prefix: true # .videos_count method
+  delegate :count, to: :similars, prefix: true # .similars_count method
   delegate :distributors, to: :company, allow_nil: true # .distributors method
+  delegate :url, :title, to: :picture, prefix: true, allow_nil: true # .picture_url, .picture_title
 
   accepts_nested_attributes_for :attrs, allow_destroy: true
   accepts_nested_attributes_for :prices, allow_destroy: true
@@ -30,12 +36,20 @@ class Product < ActiveRecord::Base
     "#{name} - #{category_name}"
   end
 
+  def date_string
+    created_at.to_date.to_s(:rfc822)
+  end
+
   def price_string
     prices.first.try(:title) # TODO: select a proper price record
   end
 
   def rating
     [ratings.average(:value).to_i, 5].min
+  end
+
+  def picture
+    pictures.first || default_picture
   end
 
   cattr_reader :default_picture do
