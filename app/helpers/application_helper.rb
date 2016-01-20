813096bffbb369a203f3b5fdbc019274aba4e921
Alexander Zagaynov <zalex80@gmail.com>
2016-01-21 06:38:25 +0700

    modal_button_tag improvements

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,16 +1,35 @@
 module ApplicationHelper
 
-  def modal_button_for action_name, button_text, **data_attrs
-    content_tag :button, type: 'button', class: 'show-modal btn btn-primary' do
-      form = capture do
-        form_tag modals_path(action_name), class: 'hidden' do
-          data_attrs.merge(action: action_name).each do |key, value|
-            concat hidden_field_tag key, value, id: nil
-          end
+  def modal_button_tag content, action = nil, **options
+    action, content = content, nil unless action
+
+    modal_mark = 'show-modal'
+    if button_class = options.delete(:class)
+      button_class.join(' ') if button_class.is_a? Array
+      button_class << " #{modal_mark}" unless button_class =~ /\b#{modal_mark}\b/
+    else
+      button_class = "#{modal_mark} btn btn-primary"
+    end
+
+    button_data = options.delete(:data) || {}
+    button_data.merge! options.extract! :disable_cache
+
+    button_options = options.extract! :type, :name
+    button_options.reverse_merge! type: :button, name: nil
+
+    button_options.merge! data: button_data
+    button_options.merge! class: button_class
+
+    options.merge! options.extract! :request_params
+
+    button_tag button_options do
+      concat content if content
+      concat capture yield if block_given?
+      capture do
+        form_tag modals_path(action), class: 'hidden' do
+          options.each { |key, value| concat hidden_field_tag key, value, id: nil }
         end
-      end
-      concat button_text
-      concat form
+      end.tap { |form| concat form }
     end
   end
 
