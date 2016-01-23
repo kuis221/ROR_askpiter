ec3bf056ad6b4683d7985f67a27af3f2400fc4d8
Alexander Zagaynov <zalex80@gmail.com>
2016-01-23 22:24:38 +0700

    thumbnails styling refactored

--- app/helpers/thumbnails_helper.rb
+++ app/helpers/thumbnails_helper.rb
@@ -1,5 +1,17 @@
 module ThumbnailsHelper
 
+  def thumbnails_container
+    raise ArgumentError, 'Missing block' unless block_given?
+
+    content_tag :div, class: 'main-container' do
+      content_tag :div, class: 'main-content-wrapper' do
+        content_tag :div, class: 'flex thumbs-matrix-wrapper' do
+          yield
+        end
+      end
+    end
+  end
+
   def product_thumbnails products
     capture { render partial: 'products/thumbnail', collection: products, as: :product }
   end
