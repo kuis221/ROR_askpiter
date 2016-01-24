25a2b1007c545614c3cdf60093f0f47fc330cf37
Alexander Zagaynov <zalex80@gmail.com>
2016-01-24 13:57:45 +0700

    auto recognize video params

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,6 +1,20 @@
 class Video < ActiveRecord::Base
   include Recentable
 
+  HOSTS = {
+    'youtube' => [/^.*(?:youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=)([^#\&\?]*).*/,
+                  'https://www.youtube.com/embed/#{code}'],
+    'vimeo'   => [/\/\/(?:www\.)?vimeo.com\/(\d+)(?:$|\/|\?)/,
+                  'https://player.vimeo.com/video/#{code}'],
+  }.freeze
+  HOSTS.each_value do |host_params|
+    host_params.freeze
+    host_params.each { |host_param| host_param.freeze }
+  end
+
+  validates :host, inclusion: { in: HOSTS.keys }, if: :url_changed?
+  before_validation :adjust_url, if: :url_changed?
+
   belongs_to :category
   belongs_to :product
   belongs_to :user
@@ -13,5 +27,29 @@ class Video < ActiveRecord::Base
 
   def self.add_billet
     new main: false, day: false
+  end
+
+  private
+
+  def url_changed?
+    changed.include? 'url'
+  end
+
+  def adjust_url
+    self.host, self.code = nil, nil
+
+    HOSTS.each do |host, host_params|
+      url_regexp, url_pattern = host_params
+
+      if matchdata = url.match(url_regexp)
+
+        self.host, self.code = host, matchdata[1]
+        self.url = url_pattern.gsub '#{code}', code
+
+        return
+      end
+    end
+
+    nil
   end
 end
