cc7b932290da242e9a0cec937cec9a45073ea827
Antoine Berton <antoine@angeltech.io>
2016-02-26 15:43:03 +0000

    add picture to sub_category

--- app/models/sub_category.rb
+++ app/models/sub_category.rb
@@ -3,6 +3,7 @@ class SubCategory < ActiveRecord::Base
   has_many :products, inverse_of: :sub_category
   has_many :filters
   has_many :filter_options, through: :filters
+  has_one :picture, as: :imageable
 
   accepts_nested_attributes_for :filters, allow_destroy: true
 end
